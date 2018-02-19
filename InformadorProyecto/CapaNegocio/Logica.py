5f168cc6d6e8f1ea7ebcadf680ab59fa58743401
sarc4 <sarca.cs@gmail.com>
2018-02-19 20:31:45 -0300

    asd
    Nueva confirmacion

---
+++ InformadorProyecto/CapaNegocio/Logica.py
@@ -0,0 +1,30 @@
+from CapaDatos.Datos import CatalogoArticulos,CatalogoDetalles,CatalogoStocks
+
+class ControladorLocal():
+
+    def __init__(self):
+        self.articulosdata = CatalogoArticulos()
+        self.detallesdata = CatalogoDetalles()
+        self.movstockdata = CatalogoStocks()
+
+
+    def getall_articulos(self):
+        return self.articulosdata.getall_articulos()
+
+    def nuevo_articulo(self,articulo):
+        self.articulosdata.insert_articulo(articulo)
+
+    def modificar_articulo(self,articulo):
+        self.articulosdata.actualizar_articulo(articulo)
+
+    def getAllDetallesSinConfirmar(self):
+        return self.detallesdata.getAllDetallesSinConfirmar()
+
+    def sumAllDetallesSinConfimrar(self):
+        return self.detallesdata.sumDetallesSinConfimrar()
+
+    def asignarIDDetallesPendientes(self,id):
+        self.detallesdata.asignarIDdetallesPendientes(id)
+
+    def SetMovimientoStock(self,movstock):
+        return self.movstockdata.setMovimientoStock(movstock)
---
+++ InformadorProyecto/CapaPresentacion/InformadorProyecto.py
@@ -0,0 +1,151 @@
+from flask import Flask,render_template,request,flash,url_for,redirect,abort,g
+from CapaDatos.modelos import db
+from werkzeug.routing import Rule
+from CapaDatos import config
+from flask_login import LoginManager,login_required,login_user,logout_user,current_user
+from CapaDatos.modelos import Usuario,Articulo, MovimientoStock,DetalleArticulo
+from CapaNegocio.Logica import ControladorLocal
+from datetime import date
+import os
+import flask_sijax
+
+app = Flask(__name__)
+
+app.debug = True
+app.url_map.add(Rule('/FormularioArticulos',endpoint='FormularioArticulos'))
+app.config.from_object(config)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+db.init_app(app)
+db.create_all(app=app)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html',pagina='inicio.html')
+
+@app.route('/Prueba')
+@login_required
+def prueba():
+    return render_template('index.html',pagina='informes.html')
+
+
+
+@login_manager.user_loader
+def load_user(id):
+    return Usuario.query.get(int(id))
+
+
+@app.route('/Login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+            return render_template('login.html')
+    usuario = request.form['usuario']
+    clave = request.form['clave']
+    usuario_registrado = Usuario.query.filter_by(nombre=usuario,clave=clave).first()
+    if usuario_registrado is None:
+            flash('Usuario o contraseña invalidos','error')
+            return abort(401)
+    login_user(usuario_registrado)
+    flash('bien logueado')
+    return redirect(request.args.get('next') or url_for('prueba'))
+
+
+@app.route('/Logout')
+def logout():
+    logout_user()
+    return redirect(url_for('hello'))
+
+
+@app.route('/Articulos')
+def articulos():
+    articulos = ControladorLocal().getall_articulos()
+    return render_template('index.html',pagina='articulos.html',articulos=articulos)
+
+
+@app.route('/FormularioArticulo')
+def formularioarticulos():
+    ID = request.args.get('ID')
+    if ID is None:
+        return render_template('index.html', pagina='FormularioArticulos.html', modificar=None, titulo='Nuevo Articulo',modo='Nuevo')
+    else:
+        art = Articulo.query.get(ID)
+        return render_template('index.html', pagina='FormularioArticulos.html', modificar=art, titulo='Modificar Articulo',modo='Modificar')
+
+
+@app.route('/registrar_articulo', methods=['POST'])
+def registrar_articulo():
+    modo = request.form['modo']
+    descripcion = request.form['descripcion']
+    precio = float(request.form['precio'])
+    stock = int(request.form['stock'])
+    if modo == 'Nuevo':
+        ControladorLocal().nuevo_articulo(Articulo(descripcion, precio, stock))
+    else:
+        id = request.form['id']
+        artmod = Articulo(descripcion, precio, stock, id)
+        ControladorLocal.modificar_articulo(artmod)
+
+    return redirect(url_for('articulos'))
+
+
+@app.route('/Personal')
+def personal():
+    personal = Usuario.query.all()
+    return render_template('index.html', pagina='personal.html', personal=personal)
+
+
+@app.route('/registrar_personal', methods=['GET', 'POST'])
+def registrar_personal():
+    nombre = request.form['usuario']
+    clave = '123'
+    habilitado = True
+    if not current_user.is_authenticated:
+        tipo = 1
+    else:
+        tipo = request.form['tipo']
+    db.session.add(Usuario(nombre,clave,habilitado,tipo))
+    db.session.commit()
+    return redirect(url_for('personal'))
+
+
+@app.route('/Stock')
+def stock():
+    detalles = MovimientoStock.query.all()
+    return render_template('index.html', pagina='stock.html', detalles=detalles)
+
+@app.route('/FormularioStock',methods=['GET','POST'])
+def formulariostock():
+        articulos = ControladorLocal().getall_articulos()
+        detalles = ControladorLocal().getAllDetallesSinConfirmar()
+        montototal = ControladorLocal().sumAllDetallesSinConfimrar()
+        return render_template('index.html',pagina='FormularioStock.html',montototal=montototal,articulos=articulos,detalles=detalles,modificar=None,titulo='Nuevo movimiento de stock')
+
+@app.route('/RegistrarStock',methods=['GET','POST'])
+def registrar_stock():
+    detalles = ControladorLocal().getAllDetallesSinConfirmar()
+    montototal = ControladorLocal().sumAllDetallesSinConfimrar()
+    fecha = date.today()
+    movstock = MovimientoStock(fecha, montototal)
+    id_generado = ControladorLocal().SetMovimientoStock(movstock)
+    ControladorLocal().asignarIDDetallesPendientes(id_generado)
+    return redirect(url_for('formulariostock'))
+
+
+@app.route('/AgregarDetalle',methods=['GET','POST'])
+def agregardetalle():
+    id = request.form['id']
+    cantidad = float(request.form['cantidad'])
+    print(cantidad)
+    precio = Articulo.query.get(id).precio_unitario
+    detalle = DetalleArticulo(0,id,precio * cantidad)
+    db.session.add(detalle)
+    db.session.commit()
+    return redirect(url_for('formulariostock'))
+
+
+if __name__ == '__main__':
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True, use_reloader=True)
